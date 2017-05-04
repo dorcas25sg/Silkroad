2d76e34b596530cc00d61d29c6a5c8fcd2114879
Dorcas <dorcas25sg@gmail.com>
2017-05-04 17:00:47 -0400

    May 4 5pm

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var dataset = "https://raw.githubusercontent.com/dorcas25sg/Silkroad/master/mapN
 var dataset2 = "https://raw.githubusercontent.com/dorcas25sg/Silkroad/master/silkroadchinese2.geojson";
 var featureGroup;
 var featureGroup2;
+var markers = [];
+
+var eachFeatureFunction = function(layer) {
+  layer.on('click', function (event) {
+     //Just for reference: to get the centriod of each polygoon: var centriod = layer.getBounds().getCenter();
+     var bounds = event.target.getBounds();
+     map.fitBounds(bounds, {padding: [50,50]});
+    console.log(layer.feature);
+  });
+};
+
+$( "#reset" ).click(function() {
+  map.setView([35.8617, 104.19], 4);
+});
 
 var popupContent = function(a, b) {
                       return '<div class="popup">' + '<h2>' + a +'</h2>'+ '<div class="slideshow">' + '<img src="' + b + '" />' + '</div>' +'</div>';
@@ -31,6 +45,16 @@ var map = L.mapbox.map('map').setView([35.8617, 104.19], 4);
                     'marker-symbol': 'museum'
                 })
              }).addTo(map).bindTooltip(popupContent(layer.feature.properties.namegeo, layer.feature.properties.urlname));
+
+             //ISSUE HERE
+             layer.on('click', function (event) {
+               console.log("Hello!");
+                //Just for reference: to get the centriod of each polygoon: var centriod = layer.getBounds().getCenter();
+                var bounds = event.target.getBounds();
+                map.fitBounds(bounds, {padding: [50,50]});
+               console.log(layer.feature);
+             });
+
 
       }});
   });
